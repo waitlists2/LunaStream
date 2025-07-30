d60d69deff8657011548987232efe562f1cc15a9
Aqua Quokka <AquaQuokka@outlook.com>
2025-07-30 23:23:19 +0800

    create app releaser

---
+++ app.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    icon: "favicon.ico",
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true
+    }
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173'); // vite dev server
+  } else {
+    win.loadFile(path.join(__dirname, 'dist/index.html')); // vite build output
+  }
+}
+
+app.whenReady().then(createWindow);
