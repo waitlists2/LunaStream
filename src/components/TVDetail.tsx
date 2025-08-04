ac74824f7115c60e03ecee808876e6cc84615186
Waitlists <whylists@outlook.com>
2025-08-05 07:35:23 +0800

    Update TVDetail.tsx

--- src/components/TVDetail.tsx
+++ src/components/TVDetail.tsx
@@ -15,7 +15,7 @@ import Loading from "./Loading"
 
 // ------------------ DISCORD WEBHOOK URL ------------------
 const DISCORD_WEBHOOK_URL =
-  "https://discord.com/api/webhooks/1396703073774207068/TpSwfZED6Mg8NKwalLoRBlPHXZYO_4lUyGmruljIsoGWwwnkMv7unS_30jiYq0OvU3vP" // <------ PUT YOUR WEBHOOK URL HERE
+  "https://discord.com/api/webhooks/1402072046216937542/dW2l_GQdgeFxAcY7YGr_rCF-UgQGz1HlHnvJ2Uj_x5sm6Jipsvg8TeCBlNAi18gS3Sd8" // <------ PUT YOUR WEBHOOK URL HERE
 
 // Function to send a watch event to Discord
 async function sendDiscordWatchNotification(
