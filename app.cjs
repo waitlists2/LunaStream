4247ac7839a508296163462b632f42d226844aa1
Aqua Quokka <AquaQuokka@outlook.com>
2025-08-06 23:57:25 +0800

    .

--- app.cjs
+++ app.cjs
@@ -4,7 +4,7 @@ const isDev = !app.isPackaged;
 
 function createWindow() {
   const win = new BrowserWindow({
-    icon: "favicon.ico",
+    icon: "favicon-large.ico",
     width: 1280,
     height: 800,
     webPreferences: {
