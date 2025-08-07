0b29790976656b4c46b86e2f42c279181d2d531f
Aqua Quokka <AquaQuokka@outlook.com>
2025-08-07 20:40:24 +0800

    add app builder

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'watch.lunastream.app',
+  appName: 'LunaStream',
+  webDir: 'dist'
+};
+
+export default config;
